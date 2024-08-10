902e1915d4939a382e749b42f96f22f20cb81bac
0obstacleo0 <0obstacleo0@gmail.com>
2024-08-10 22:47:10 +0900

    コンポーネント追加

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,44 @@
+import {
+  Bookmark,
+  Home,
+  Notifications,
+  Person,
+  Search,
+  Settings,
+} from "@mui/icons-material";
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <Home className="sidebarIcon" />
+            <span className="sidebarListItemText">ホーム</span>
+          </li>
+          <li className="sidebarListItem">
+            <Search className="sidebarIcon" />
+            <span className="sidebarListItemText">検索</span>
+          </li>
+          <li className="sidebarListItem">
+            <Notifications className="sidebarIcon" />
+            <span className="sidebarListItemText">通知</span>
+          </li>
+          <li className="sidebarListItem">
+            <Bookmark className="sidebarIcon" />
+            <span className="sidebarListItemText">ブックマーク</span>
+          </li>
+          <li className="sidebarListItem">
+            <Person className="sidebarIcon" />
+            <span className="sidebarListItemText">プロフィール</span>
+          </li>
+          <li className="sidebarListItem">
+            <Settings className="sidebarIcon" />
+            <span className="sidebarListItemText">設定</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
