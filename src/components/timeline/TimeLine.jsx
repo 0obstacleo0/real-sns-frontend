a59a496b10f983d3514b8a072ed061fd87f7ed96
0obstacleo0 <0obstacleo0@gmail.com>
2024-08-12 11:20:47 +0900

    postコンポーネント追加

--- src/components/timeline/TimeLine.jsx
+++ src/components/timeline/TimeLine.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Share from "../share/Share";
 import "./Timeline.css";
+import Post from "../post/Post";
 
 export default function Timeline() {
   return (
     <div className="timeline">
       <div className="timelineWrapper">
         <Share />
+        <Post />
       </div>
     </div>
   );
