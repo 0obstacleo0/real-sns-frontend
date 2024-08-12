76d8866e4623f29587fe1fdd1ef54925559aad56
0obstacleo0 <0obstacleo0@gmail.com>
2024-08-12 11:44:32 +0900

    post css 修正

--- src/components/timeline/TimeLine.jsx
+++ src/components/timeline/TimeLine.jsx
@@ -9,6 +9,11 @@ export default function Timeline() {
       <div className="timelineWrapper">
         <Share />
         <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </div>
     </div>
   );
