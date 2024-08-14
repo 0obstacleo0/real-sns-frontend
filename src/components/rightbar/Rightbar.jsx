2d75dd5392202435b421c7efcd09dff10cc6d0b2
0obstacleo0 <0obstacleo0@gmail.com>
2024-08-15 08:55:24 +0900

    RIghtbar 修正

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,10 +3,10 @@ import "./Rightbar.css";
 import Online from "../online/Online";
 import { Users } from "../../dummyData";
 
-export default function Rightbar() {
-  return (
-    <div className="rightbar">
-      <div className="rightbarWrapper">
+export default function Rightbar({ profile }) {
+  const HomeRightbar = () => {
+    return (
+      <>
         <div className="eventContainer">
           <img src="/assets/star.png" alt="" className="starImg" />
           <span className="eventText">
@@ -39,6 +39,63 @@ export default function Rightbar() {
           className="rightbarPromotionImg"
         />
         <p className="promotionName">株式会社</p>
+      </>
+    );
+  };
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">ユーザー情報</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">出身：</span>
+            <span className="rightbarInfoKey">福岡</span>
+          </div>
+        </div>
+        <h4 className="rightbarTitle">あなたの友達</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img
+              src="/assets/person/1.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">shin code</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="/assets/person/1.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">shin code</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="/assets/person/1.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">shin code</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="/assets/person/1.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">shin code</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        {profile ? <ProfileRightbar /> : <HomeRightbar />}
       </div>
     </div>
   );
